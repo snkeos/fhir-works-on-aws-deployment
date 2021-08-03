9168167c444f7cc4c5c371ebd8cde64fef273e39
MarioDegen <51999287+MarioDegen@users.noreply.github.com>
2021-08-03 17:20:34 +0200

    BackMerge of MultiTenancy

--- scripts/win_install.ps1
+++ scripts/win_install.ps1
@@ -5,8 +5,12 @@
 param (
     [string]$region = "us-west-2",
     [string]$stage = "dev",
+    [switch]$multiTenancyEnabled = $false,
+    [switch]$multiTenancyTenantSubUrlEnabled = $false,
+    [string]$multiTenancyTokenClaim = "",
+    [string]$multiTenancyTokenClaimValuePrefix = "",
     [switch]$help = $false
- )
+)
 
 ##Usage information
 function Usage {
@@ -16,27 +20,31 @@ function Usage {
     Write-Host "Optional Parameters:`n"
     Write-Host "    -stage: Set stage for deploying AWS services (Default: 'dev')"
     Write-Host "    -region: Set region for deploying AWS services (Default: 'us-west-2')"
+    Write-Host "    -multiTenancyEnabled: If set, multi tenancy is enabled."
+    Write-Host "    -multiTenancyTenantSubUrlEnabled: If set, the tenant sub url is in the path: /tenant/{tenantId}/Patient/... otherwise /{tenantId}/Patient/..."
+    Write-Host "    -multiTenancyTokenClaim: Set the access token claim which is used to grant/deny the access on a particular tenant data pool. If no claim is set tenant base access control is disabled (Default: '')"
+    Write-Host "    -multiTenancyTokenClaimValuePrefix: If a claim is used, which also not only contains tenant related values (e.g. 'cognito:groups'), an optional tenant prefix for matching can be specified (Default: '')"
     Write-Host "    -help: Displays this message`n`n"
 }
 
 #Refreshes powershell environment without needing to reopen powershell
 function Refresh-Environment {
-    foreach($level in "Machine","User") {
-       [Environment]::GetEnvironmentVariables($level).GetEnumerator() | % {
-          # For Path variables, append the new values, if they're not already in there
-          if($_.Name -match 'Path$') { 
-             $_.Value = ($((Get-Content "Env:$($_.Name)") + ";$($_.Value)") -split ';' | Select -unique) -join ';'
-          }
-          $_
-       } | Set-Content -Path { "Env:$($_.Name)" }
+    foreach ($level in "Machine", "User") {
+        [Environment]::GetEnvironmentVariables($level).GetEnumerator() | % {
+            # For Path variables, append the new values, if they're not already in there
+            if ($_.Name -match 'Path$') { 
+                $_.Value = ($((Get-Content "Env:$($_.Name)") + ";$($_.Value)") -split ';' | Select -unique) -join ';'
+            }
+            $_
+        } | Set-Content -Path { "Env:$($_.Name)" }
     }
 }
 
 function Install-Dependencies {
     #Dependencies:
-        #   nodejs  ->  npm   -> serverless
-        #           ->  yarn
-        #   python3 ->  boto3
+    #   nodejs  ->  npm   -> serverless
+    #           ->  yarn
+    #   python3 ->  boto3
     $dep_missing = $false
     Write-Host "The following dependencies will need to be installed: "
     Get-Command node 2>&1 | out-null
@@ -45,7 +53,7 @@ function Install-Dependencies {
     if (-Not ($?)) { Write-Host "  - python3"; $dep_missing = $true }
     Get-Command yarn 2>&1 | out-null
     if (-Not ($?)) { Write-Host "  - yarn"; $dep_missing = $true }
-    if (-Not ($dep_missing)){
+    if (-Not ($dep_missing)) {
         Write-Host "`nNone! All dependencies already satisfied"
         Write-Host "We just need to double-check that the boto3 python module is installed..."
         python -m pip install boto3
@@ -64,7 +72,7 @@ function Install-Dependencies {
         }
     } until ($response -eq 0)
 
-    if (-Not (Get-Command choco 2>&1 | out-null)){
+    if (-Not (Get-Command choco 2>&1 | out-null)) {
         Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
     }
     choco install -y nodejs.install --version=12.18.3 #also installs npm by default
@@ -80,7 +88,8 @@ function Install-Dependencies {
     if (-Not (Get-Command yarn 2>&1 | out-null)) { 
         $newpath = "$newpath;C:\Program Files (x86)\Yarn\bin"
     }
-    if (-Not (Get-Command python 2>&1 | out-null)) { #this really should never happen
+    if (-Not (Get-Command python 2>&1 | out-null)) {
+        #this really should never happen
         $newpath = "$newpath;C:\Python38;C:\Python38\scripts" 
     }
     Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newpath
@@ -102,42 +111,47 @@ function Install-Dependencies {
 ## Note: This function only reads single lines of YAML files
 function GetFrom-Yaml {
     Param($valName)
-    gc Info_Output.yml | % { if($_ -match "^[`t` ]*$valName") {Return $_.split(": ")[-1]}}
+    gc Info_Output.yml | % { if ($_ -match "^[`t` ]*$valName") { Return $_.split(": ")[-1] } }
 }
 
 function Get-ValidPassword {
     $specialPattern = "[" + [regex]::Escape("~!@#$%^&()-.+=}{\/|;:<>?'*`"") + "]"
 
-    $matched=$true
-    for (;;) {
+    $matched = $true
+    for (; ; ) {
         #I broke this up into an if/elseif chain to give more detailed feedback to the user.
         #This could be condensed into a single large conditional check.
         if (-Not ($matched)) {
             Write-Host "`nERROR: Passwords did not match. Please try again.`n"
-            $matched=$true           
+            $matched = $true           
         }
         $s1 = Read-Host -AsSecureString "Enter Password " 
         $s1 = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($s1))
         #^above line is needed to convert hidden string to text for analysis
-        if  (($s1.length -lt 8) -Or ($s1.length -gt 20)){
+        if (($s1.length -lt 8) -Or ($s1.length -gt 20)) {
             Write-Host "`n`n`n`n`n`n`n`n`n`n`n`n"
             Write-Host "`nERROR: Password does not meet required length (8-20 characters).`n`n"
-        } elseif (-Not (($s1 -cmatch "[A-Z]") -And ($s1 -cmatch "[a-z]"))) {
+        }
+        elseif (-Not (($s1 -cmatch "[A-Z]") -And ($s1 -cmatch "[a-z]"))) {
             Write-Host "`n`n`n`n`n`n`n`n`n`n`n`n"
             Write-Host "`nERROR: Password must contain at least one uppercase and lowercase character.`n`n"
-        } elseif (-Not ($s1 -match "\d")){
+        }
+        elseif (-Not ($s1 -match "\d")) {
             Write-Host "`n`n`n`n`n`n`n`n`n`n`n`n"
             Write-Host "`nERROR: Password must contain at least one number.`n`n"
-        } elseif (-Not ($s1 -match $specialPattern)){
+        }
+        elseif (-Not ($s1 -match $specialPattern)) {
             Write-Host "`n`n`n`n`n`n`n`n`n`n`n`n"
             Write-Host "`nERROR: Password must contain at least one special character.`nSpecial characters: ~!@#$%^&()-.+=}{\/|;:<>?'*`""
-        } else {
+        }
+        else {
             $s2 = Read-Host -AsSecureString "Please confirm your password "
             $s2 = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($s2))
-            if (-Not ($s1 -eq $s2)){
-                $matched=$false
+            if (-Not ($s1 -eq $s2)) {
+                $matched = $false
                 Continue
-            } else {
+            }
+            else {
                 Break
             }
         }
@@ -151,6 +165,29 @@ function Get-ValidPassword {
     }
 
     Return $s1
+}
+function BuildServerlessCommandLineArgs($serverlessActions, $tokenClaim, $tokenClaimValuePrefix, $tenantSubUrl) {
+    [string[]] $runServerlessArgs = @("run", "serverless")	
+    [string[]] $runServerlessParamsArgs = @("--region", $region, "--stage", $stage)	
+    if ( $multiTenancyEnabled ) {
+        $runServerlessParamsArgs += ' --useMultiTenancy true'
+    
+        if ( $tokenClaim -ne "" ) {  
+            $runServerlessParamsArgs += '--multiTenancyTokenClaim'
+            $runServerlessParamsArgs += $tokenClaim
+    
+            if ( $tokenClaimValuePrefix -ne "" ) {
+                $runServerlessParamsArgs += '--multiTenancyTokenClaimValuePrefix' 
+                $runServerlessParamsArgs += $tokenClaimValuePrefix
+            }
+        }
+    
+        if ( $tenantSubUrl ) {
+            $runServerlessParamsArgs += ' --useMultiTenancyTenantSubUrl true'
+        }
+    }
+    [string[]] $compositeCmdArgs = @($runServerlessArgs, $serverlessActions, $runServerlessParamsArgs)
+    Return $compositeCmdArgs
 }
 
 function Credentials-Error {
@@ -168,12 +205,12 @@ function Credentials-Error {
 #####################
 Import-Module AWSPowerShell
 Get-AWSPowerShellVersion 2>&1 | out-null
-if (-Not ( $? ) ){
+if (-Not ( $? ) ) {
     Write-Host "ERROR: AWS Powershell is not installed. Please install and try again."
     Exit
 }
 
-if ($help){
+if ($help) {
     Usage
     Exit
 }
@@ -208,16 +245,17 @@ if ($response -eq 1) {
 Get-CFNStack -StackName fhir-service-$stage -Region $region 2>&1 | out-null
 $already_deployed = $?
 
-if ($already_deployed){
+if ($already_deployed) {
     $redep = (Get-CFNStack -StackName -Region $region fhir-service-$stage)
-    if ( Write-Output "$redep" | Select-String "DELETE_FAILED" ){
+    if ( Write-Output "$redep" | Select-String "DELETE_FAILED" ) {
         #This would happen if someone tried to delete the stack from the AWS Console
         #This leads to a situation where the stack is half-deleted, and needs to be removed with `serverless remove`
-        $fail=$true
+        $fail = $true
         $msg = "ERROR: FHIR Server already exists, but it seems to be corrupted.`nWould you like to remove the current installation and redeploy the FHIR Server?`n"
 
-    } else {
-        $fail=$false
+    }
+    else {
+        $fail = $false
         $msg = "Do you want to redeploy the server?"
     }
 
@@ -234,7 +272,16 @@ if ($already_deployed){
 
 Write-Host "Setup will proceed with the following parameters: `n"
 Write-Host "  Stage: $stage"
-Write-Host "  Region: $region`n`n"
+Write-Host "  Region: $region"
+
+Write-Host "  Multi Tenancy Enabled: $multiTenancyEnabled"
+if ($multiTenancyEnabled) {
+    Write-Host "  Multi Tenancy Tenant Sub Url Enabled: $multiTenancyTenantSubUrlEnabled"
+    Write-Host "  Multi Tenancy Access Control Token Claim: $multiTenancyTokenClaim"
+    Write-Host "  Multi Tenancy Access Control Token Claim Value Prefix: $multiTenancyTokenClaimValuePrefix"
+}
+Write-Host "`n`n"
+
 do {
     $response = $Host.UI.PromptForChoice("", "Are these settings correct?", $options, $default)
     if ($response -eq 1) {
@@ -247,7 +294,7 @@ do {
 Write-Host "`nInstalling dependencies...`n"
 Install-Dependencies
 
-$IAMUserARN=(Get-STSCallerIdentity).Arn
+$IAMUserARN = (Get-STSCallerIdentity).Arn
 
 Set-Location $rootDir
 yarn install --frozen-lockfile
@@ -260,13 +307,15 @@ yarn run test
 ## Deploy using profile to stated region
 fc >> serverless_config.json
 $SEL = Select-String -Path serverless_config.json -Pattern "devAwsUserAccountArn"
-if ($SEL -eq $null){ 
+if ($SEL -eq $null) { 
     Add-Content -Path serverless_config.json -Value "`n{`n  `"devAwsUserAccountArn`": `"$IAMUserARN`"`n}"
 }
 
 Write-Host "`n`nDeploying FHIR Server"
 Write-Host "(This may take some time, usually ~20-30 minutes)`n`n" 
-yarn run serverless deploy --region $region --stage $stage
+
+$deployArgs = BuildServerlessCommandLineArgs "deploy" $multiTenancyTokenClaim $multiTenancyTokenClaimValuePrefix $multiTenancyTenantSubUrlEnabled
+Start-Process yarn -ArgumentList $deployArgs -wait -NoNewWindow -PassThru
 
 if (-Not ($?) ) {
     Write-Host "Setting up FHIR Server failed. Please try again later."
@@ -276,7 +325,9 @@ Write-Host "Deployed Successfully.`n"
 
 rm Info_Output.yml
 fc >> Info_Output.yml
-yarn run serverless info --verbose --region $region --stage $stage | Out-File -FilePath .\Info_Output.yml
+$infoArgs = BuildServerlessCommandLineArgs [string[]]@("info", "--verbose") $multiTenancyTokenClaim $multiTenancyTokenClaimValuePrefix $multiTenancyTenantSubUrlEnabled
+Start-Process yarn -ArgumentList $infoArgs -wait -NoNewWindow -PassThru -RedirectStandardOutput .\Info_Output.yml
+#yarn run serverless info --verbose --region $region --stage $stage $mtArgs | Out-File -FilePath .\Info_Output.yml
 
 #Read in variables from Info_Output.yml
 $UserPoolId = GetFrom-Yaml "UserPoolId"
@@ -298,44 +349,50 @@ Write-Host "`n***`n"
 
 #CHECK
 python provision-user.py "$UserPoolId" "$UserPoolAppClientId" "$region"
-if (-Not ($?)){
+if (-Not ($?)) {
     Write-Host "Warning: Cognito has already been initialized.`nIf you need to generate a new token, please use the init-auth.py script.`nContinuing..."
 }
 Write-Host "`n***`n`n"
 
 
 # #Set up Cognito user for Kibana server
-if ($stage -eq "dev"){
+if ($stage -eq "dev") {
     Write-Host "In order to be able to access the Kibana server for your ElasticSearch Service Instance, you need create a cognito user."
     Write-Host "You can set up a cognito user automatically through this install script, `nor you can do it manually via the Cognito console.`n"
-    for(;;) {
+    for (; ; ) {
         $yn = $Host.UI.PromptForChoice("", "Do you want to set up a cognito user now?", $options, $default)
-        if ($yn -eq 1) { #no
-            $resp=$false
+        if ($yn -eq 1) {
+            #no
+            $resp = $false
             Break
-        } elseif ($yn -eq 0){ #yes
-            $resp=$true
+        }
+        elseif ($yn -eq 0) {
+            #yes
+            $resp = $true
             Break
         }
     } 
-    while ($resp){
+    while ($resp) {
         Write-Host ""
         Write-Host "Okay, we'll need to create a cognito user using an email address and password."
         Write-Host ""
         $cognitoUsername = Read-Host "Enter your email address (<youremail@address.com>): "
         
-        for(;;) {
+        for (; ; ) {
             $yn = $Host.UI.PromptForChoice("", "`n`nIs $cognitoUsername your correct email?`n", $options, $default)
-            if ($yn -eq 1) { #no
-                $check=$false
+            if ($yn -eq 1) {
+                #no
+                $check = $false
                 Break
-            } elseif ($yn -eq 0){ #yes
-                $check=$true
+            }
+            elseif ($yn -eq 0) {
+                #yes
+                $check = $true
                 Break
             }
         }
 
-        if ($check){
+        if ($check) {
             Write-Host "`n`nPlease create a temporary password. Passwords must satisfy the following requirements: "
             Write-Host "  * 8-20 characters long"
             Write-Host "  * at least 1 lowercase character"
@@ -346,8 +403,8 @@ if ($stage -eq "dev"){
             $temp_cognito_p = Get-ValidPassword
             Write-Host ""
             Register-CGIPUserInPool -Username $cognitoUsername `
-             -Password $temp_cognito_p -ClientId $ElasticSearchKibanaUserPoolAppClientId `
-             -Region $region -UserAttribute @{Name="email";Value="$cognitoUsername"}
+                -Password $temp_cognito_p -ClientId $ElasticSearchKibanaUserPoolAppClientId `
+                -Region $region -UserAttribute @{Name = "email"; Value = "$cognitoUsername" }
             if ( $? ) {
                 Write-Host "`nSuccess: Created a cognito user.`n`n \
                 You can now log into the Kibana server using the email address you provided (username) and your temporary password.`n \
@@ -358,16 +415,20 @@ if ($stage -eq "dev"){
             }
             Break
 
-        } else {
+        }
+        else {
             Write-Host "`nSorry about that--let's start over.`n"
             Write-Host "Do you want to set up a cognito user now?"
-            for(;;) {
+            for (; ; ) {
                 $yn = $Host.UI.PromptForChoice("", "Do you want to set up a cognito user now?", $options, $default)
-                if ($yn -eq 1) { #no
-                    $resp=$false
+                if ($yn -eq 1) {
+                    #no
+                    $resp = $false
                     Break
-                } elseif ($yn -eq 0){ #yes
-                    $resp=$true
+                }
+                elseif ($yn -eq 0) {
+                    #yes
+                    $resp = $true
                     Break
                 }
             } 
@@ -378,14 +439,18 @@ if ($stage -eq "dev"){
 
 Write-Host "\nYou can also set up the server to archive logs older than 7 days into S3 and delete those logs from Cloudwatch Logs."
 Write-Host "You can also do this later manually, if you would prefer."
-for(;;) {
+for (; ; ) {
     $yn = $Host.UI.PromptForChoice("", "`n`nWould you like to set the server to archive logs older than 7 days into S3?`n", $options, $default)
-    if ($yn -eq 1) { #no
+    if ($yn -eq 1) {
+        #no
         Break
-    } elseif ($yn -eq 0){ #yes
+    }
+    elseif ($yn -eq 0) {
+        #yes
         Set-Location $rootDir\auditLogMover
         yarn install --frozen-lockfile
-        yarn run serverless deploy --region $region --stage $stage
+        Start-Process yarn -ArgumentList $deployArgs -wait -NoNewWindow -PassThru
+        #yarn run serverless deploy --region $region --stage $stage
         Set-Location $rootDir
         Write-Host "`n`nSuccess."
         Break
@@ -399,11 +464,14 @@ Write-Host "DynamoDB Table backups can also be set up later. See the README file
 Write-Host "Note: This will deploy an additional stack, and can lead to increased costs to run this server."
 Write-Host ""
 Write-Host "Would you like to set up backups now?`n"
-for(;;) {
+for (; ; ) {
     $yn = $Host.UI.PromptForChoice("", "Would you like to set up backups now?`n", $options, $default)
-    if ($yn -eq 1) { #no
+    if ($yn -eq 1) {
+        #no
         Break
-    } elseif ($yn -eq 0){ #yes
+    }
+    elseif ($yn -eq 0) {
+        #yes
         New-CFNStack -StackName fhir-server-backups -Region $region -TemplateBody (Get-Content -Raw .\cloudformation\backup.yaml) -Capability CAPABILITY_NAMED_IAM
         if ( $? ) {
             Write-Host "DynamoDB Table backups were set up successfully."
