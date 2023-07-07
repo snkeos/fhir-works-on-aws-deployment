83b4d166b90d78896d3d25816b1e69a3ba13a518
Christopher Watanabe <christopher.watanabe@brainlab.com>
2023-07-07 08:55:45 +0200

    fix: List all params in args arrays

--- scripts/install.sh
+++ scripts/install.sh
@@ -467,7 +467,7 @@ else
 fi
 
 useApiKeysArgs=(--param="useApiKeys=$apiKeysEnabled")
-yarn run serverless deploy --region $region --stage $stage "$stageTypeArgs" "$extUserPoolArgs" "$mtArgs" "$corsOriginsArgs" "$useApiKeysArgs" --verbose || { echo >&2 "Failed to deploy serverless application."; exit 1; }
+yarn run serverless deploy --region $region --stage $stage "${stageTypeArgs[@]}" "${extUserPoolArgs[@]}""${mtArgs[@]}" "${corsOriginsArgs[@]}" "${useApiKeysArgs[@]}" --verbose || { echo >&2 "Failed to deploy serverless application."; exit 1; }
 
 ## Output to console and to file Info_Output.log.  tee not used as it removes the output highlighting.
 echo -e "Deployed Successfully.\n"
