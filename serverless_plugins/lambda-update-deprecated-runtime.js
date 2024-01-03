7934c8a6ee43805c2df34aa6e1da41139dcc6ff6
Mario Degenhardt <mario.degenhardt@snkeos.com>
2024-01-03 09:23:46 +0100

    fix: change UpdateRuntimeOn from Auto to FunctionUpdate

--- serverless_plugins/lambda-update-deprecated-runtime.js
+++ serverless_plugins/lambda-update-deprecated-runtime.js
@@ -5,25 +5,23 @@ class LambdaUpdateDeprecatedRuntime {
         this.provider = 'aws';
 
         this.hooks = {
-            'before:deploy:deploy': this.afterCompileEvents.bind(this),
+            'before:deploy:deploy': this.beforeDeploy.bind(this),
         };
         console.log('LambdaUpdateDeprecatedRuntime::cstor');
     }
 
-    afterCompileEvents() {
-        console.log('LambdaUpdateDeprecatedRuntime::afterCompileEvents');
-        this.serverless.cli.log('LambdaUpdateDeprecatedRuntime::afterCompileEvents');
+    beforeDeploy() {
+        this.serverless.cli.log('LambdaUpdateDeprecatedRuntime::before:deploy:deploy');
 
         const key = 'CustomDashresourceDashapigwDashcwDashroleLambdaFunction';
         let resources = this.serverless.service.provider.compiledCloudFormationTemplate.Resources;
-        if (
-            key in resources /* &&
-            (resources[key].Properties.Runtime === 'nodejs12.x' ||
-                resources[key].Properties.Runtime === 'nodejs14.x' ||
-                resources[key].Properties.Runtime === 'nodejs16.x') */
-        ) {
+        if (key in resources) {
             resources[key].Properties.Runtime = 'nodejs18.x';
             this.serverless.cli.log('Fixed runtime to `nodejs18.x`');
+            resources[key].Properties.RuntimeManagementConfig = {
+                UpdateRuntimeOn: 'FunctionUpdate',
+            };
+            this.serverless.cli.log('Fixed RuntimeManagementConfig: `UpdateRuntimeOn` to `FunctionUpdate`');
         }
     }
 }
