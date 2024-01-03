a5d35fe39a6ee34bbd17c2878303c2a19ae18397
Mario Degenhardt <mario.degenhardt@snkeos.com>
2024-01-03 09:00:38 +0100

    fix: only patch CustomDashresourceDashapigwDashcwDashroleLambdaFunction

--- serverless_plugins/lambda-update-deprecated-runtime.js
+++ serverless_plugins/lambda-update-deprecated-runtime.js
@@ -14,26 +14,17 @@ class LambdaUpdateDeprecatedRuntime {
         console.log('LambdaUpdateDeprecatedRuntime::afterCompileEvents');
         this.serverless.cli.log('LambdaUpdateDeprecatedRuntime::afterCompileEvents');
 
-        let resources = this.serverless.service.provider.compiledCloudFormationTemplate.Resources;
-
-        Object.keys(resources).forEach((resourceKey) => {
-            resources[resourceKey].Properties.Runtime = 'nodejs18.x'; // or: 'nodejs16.x'
-        });
-
-        /*
-        let key = 'CustomDashresourceDashapigwDashcwDashroleLambdaFunction';
+        const key = 'CustomDashresourceDashapigwDashcwDashroleLambdaFunction';
         let resources = this.serverless.service.provider.compiledCloudFormationTemplate.Resources;
         if (
             key in resources /* &&
             (resources[key].Properties.Runtime === 'nodejs12.x' ||
                 resources[key].Properties.Runtime === 'nodejs14.x' ||
-                resources[key].Properties.Runtime === 'nodejs16.x') 
+                resources[key].Properties.Runtime === 'nodejs16.x') */
         ) {
             resources[key].Properties.Runtime = 'nodejs18.x';
-            this.serverless.cli.log(
-                'Fixed CustomDashresourceDashapigwDashcwDashroleLambdaFunction runtime to `nodejs18.x`' );
-           
-        } */
+            this.serverless.cli.log('Fixed runtime to `nodejs18.x`');
+        }
     }
 }
 
