af1228834ee247a95780f6c7e149ed0ee607756e
Christopher Watanabe <christopher.watanabe@snke.com>
2025-08-20 16:42:52 +0200

    fix: Ignore node_modules in eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,9 @@
  */
 
 module.exports = {
+    ignorePatterns: [
+        "node_modules/",
+    ],
     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
     extends: [
         'airbnb-base',
